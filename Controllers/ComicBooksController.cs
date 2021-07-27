4096c840b3d35e142cf57d116f9c88c515812b6c
dbellefeuille <dtb1996@yahoo.com>
2021-07-27 14:11:48 -0400

    added detail action method

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace WebApplication1.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller";
+        }
     }
 }
